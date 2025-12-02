074b193b4b0b2c7ec34484e860eb06df7caef9a1
jtowne01 <151704673+jtowne01@users.noreply.github.com>
2025-12-02 18:51:35 -0500

    Update auth.js

--- mern/backend/routes/auth.js
+++ mern/backend/routes/auth.js
@@ -70,4 +70,14 @@ router.post('/login', async (req, res) => {
 
 }); 
 
+router.get('/me', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        res.json(user);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
+
 module.exports = router;
