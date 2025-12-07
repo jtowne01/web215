ec963bb76435f1d22e897537200c07113b1963ec
jtowne01 <151704673+jtowne01@users.noreply.github.com>
2025-12-07 18:49:57 -0500

    Update Register.js

--- mern/frontend/my-react-app/src/components/Register.js
+++ mern/frontend/my-react-app/src/components/Register.js
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 const Register = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [message, setMessage] = useState("");
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
@@ -55,5 +56,6 @@ const Register = () => {
 };
 
 export default Register;
+
 
 
